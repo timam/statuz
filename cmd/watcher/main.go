d25300a9d744b5d281147a5d0ec903dbbe5c4e6f
Timam <mail@timam.io>
2023-09-22 01:32:07 +0600

    added a init log

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/timam/statuz/internal/healthz"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("starting statuz watcher")
 	http.HandleFunc("/healthz", healthz.HealthCheck)
 	http.ListenAndServe(":8080", nil)
 }
