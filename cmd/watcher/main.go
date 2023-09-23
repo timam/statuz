131605ce98ccc4a53563f3aea174827c82464f4c
Timam <mail@timam.io>
2023-09-23 13:35:57 +0600

    getting env vars

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"github.com/timam/statuz/config"
 	"github.com/timam/statuz/internal/healthz"
 	"log"
 	"net/http"
 )
 
 func main() {
+	_, err := config.GetEnvVars()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	log.Println("starting statuz watcher")
+
 	http.HandleFunc("/healthz", healthz.HealthCheck)
 	http.ListenAndServe(":8080", nil)
 }
---
+++ config/env.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type EnvVars struct {
+	Type     string
+	Endpoint string
+	Interval string
+}
+
+func GetEnvVars() (*EnvVars, error) {
+	envVars := &EnvVars{
+		Type:     os.Getenv("TYPE"),
+		Endpoint: os.Getenv("ENDPOINT"),
+		Interval: os.Getenv("INTERVAL"),
+	}
+
+	if envVars.Type == "" {
+		return nil, errors.New("TYPE environment variable is not set")
+	}
+
+	if envVars.Endpoint == "" {
+		return nil, errors.New("ENDPOINT environment variable is not set")
+	}
+
+	if envVars.Type != "url" && envVars.Type != "ip" {
+		return nil, errors.New("TYPE must be 'url' or 'ip'")
+	}
+
+	if !isValidURL(envVars.Endpoint) && !isValidIP(envVars.Endpoint) {
+		return nil, errors.New("ENDPOINT must be a valid URL or IP address")
+	}
+
+	if strings.HasSuffix(envVars.Interval, "s") {
+		envVars.Interval = strings.TrimSuffix(envVars.Interval, "s")
+	}
+
+	if envVars.Interval == "" {
+		envVars.Interval = "300"
+	}
+
+	printEnvs(envVars)
+
+	return envVars, nil
+}
+
+func isValidURL(s string) bool {
+	regex := `^(https?):\/\/[^\s/$.?#].[^\s]*$`
+	return regexp.MustCompile(regex).MatchString(s)
+}
+
+func isValidIP(s string) bool {
+	regex := `^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$`
+	return regexp.MustCompile(regex).MatchString(s)
+}
+
+func printEnvs(env *EnvVars) {
+	message := fmt.Sprintf("Type: %s, Endpoint: %s, Interval: %s", env.Type, env.Endpoint, env.Interval)
+	log.Println(message)
+}
