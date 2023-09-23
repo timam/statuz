ac575abf072e800be5b5363d71b0a46a79a97048
Timam <mail@timam.io>
2023-09-23 13:47:41 +0600

    created a seperate package for watcher

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -1,20 +1,13 @@
-package main
+package watcher
 
 import (
 	"github.com/timam/statuz/config"
-	"github.com/timam/statuz/internal/healthz"
 	"log"
-	"net/http"
 )
 
-func main() {
+func Start() {
 	_, err := config.GetEnvVars()
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	log.Println("starting statuz watcher")
-
-	http.HandleFunc("/healthz", healthz.HealthCheck)
-	http.ListenAndServe(":8080", nil)
 }
