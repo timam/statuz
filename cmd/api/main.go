1b765d4a5f3cef7f6eda2e8b153e5eb914316d6b
hiroksarker <hirok.sarker@gmail.com>
2023-09-24 12:32:12 +0600

    Error handle and code refactor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/timam/statuz/cmd/watcher"
 	"github.com/timam/statuz/internal/healthz"
-	"net/http"
 )
 
 func main() {
-	watcher.Start()
+	err := watcher.Start()
+	if err != nil {
+		log.Fatalf("Error starting watcher: %v", err)
+	}
 
 	http.HandleFunc("/healthz", healthz.HealthCheck)
 	http.ListenAndServe(":8080", nil)
