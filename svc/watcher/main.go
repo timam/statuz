87882e4081f124366e79ae771be473559b638411
Timam <mail@timam.io>
2023-10-03 13:04:54 +0600

    added waitgroup to run watcher and http

--- svc/watcher/main.go
+++ svc/watcher/main.go
@@ -7,17 +7,38 @@ import (
 	"github.com/statuzproj/statuz/utils/healthz"
 	"log"
 	"net/http"
+	"sync"
 )
 
 func main() {
-	err := start()
-	if err != nil {
-		log.Fatalf("Error starting watcher: %v", err)
-	}
+	var wg sync.WaitGroup
 
-	http.HandleFunc("/healthz", healthz.HealthCheck)
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8080", nil)
+	// Start the HTTP server in a goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		http.HandleFunc("/healthz", healthz.HealthCheck)
+		http.Handle("/metrics", promhttp.Handler())
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+	}()
+
+	// Start the watcher in another goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		err := start()
+		if err != nil {
+			log.Fatalf("Error starting watcher: %v", err)
+		}
+	}()
+
+	// Wait for all goroutines to complete before exiting
+	wg.Wait()
 }
 
 func start() error {
