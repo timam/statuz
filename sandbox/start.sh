25bf329fa59ad7403d4e0c65188da99cb9bb330f
Timam <mail@timam.io>
2023-10-22 22:15:44 +0600

    cleanup dependency

--- sandbox/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start kubectl proxy in the background
-kubectl proxy --address='0.0.0.0' --port='18001' --disable-filter=true &
-
-# Start Docker Compose services
-docker-compose up -d
